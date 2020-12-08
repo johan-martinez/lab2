da8e762a5ed3742b93e213f5ada3e977a75b73ca
johan-martinez <johan.martinez01@uptc.edu.co>
2020-12-08 13:00:21 -0500

    Traslado el cliente a React

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+
+class App extends Component{
+  constructor(){
+    super();
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
+}
+
+render(<App/>,document.getElementById('root'));
