f81112e1b0e7c860680da2556cb65eebc7834889
johan-martinez <johan.martinez01@uptc.edu.co>
2020-12-10 22:34:41 -0500

    Se realizó el server y el cliente(React)

--- server/index.js
+++ server/index.js
@@ -1,29 +1,83 @@
-
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs')
-const exec = require('child_process').exec;
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs');
+const jimp = require('jimp')
+var port = process.env.PORT || 5000;
 
+//Multer configuration
+const storage=multer.diskStorage({
+    destination:path.join(__dirname,'/images/uploads'),
+    filename:(req,file,cb)=>{
+    cb(null,file.originalname)
+}})
 
-const pathFile = 'logs/latest.log';
-const restart_server_file = 'restart_server.sh';
-const serversUrls = [{ ip: "192.168.1.72", port: 3000 }, { ip: "192.168.1.73", port: 3000 }];
-
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-var express = require('express')
-var cors = require('cors')
-
-const app = express();
-const port = process.env.PORT || 3000;
+var app=express()
 
 app.use(cors());
-app.use(express.json())
+app.use(bodyParser.json())
 
-app.get('/',(req,res)=>{
-    res.sendStatus(200)
+app.use(multer({
+    storage,
+    dest:path.join(__dirname,'/images')
+}).single('image'))
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+function sendImage (res,p) {
+    var dir = path.join(__dirname, 'images/transform');
+    var file = path.join(dir, p);
+    if (file.indexOf(dir + path.sep) !== 0) {
+        return res.status(403).end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.set('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('close',function () {
+        res.end()
+        s.close()
+        fs.unlinkSync(`images/transform/${p}`)
+    })
+    s.on('error', function () {
+        res.set('Content-Type', 'text/plain');
+        res.status(404).end('Not found');
+    });
+}
+
+app.post('/',(req,res)=>{
+    if (req.file) {
+        jimp.read(req.file.path)
+        .then(function (image) {
+            loadedImage = image;
+            return jimp.loadFont(jimp.FONT_SANS_32_WHITE)
+        })
+        .then(async function (font) {
+            let date= Date.now()
+            await loadedImage.print(font, parseInt(10), parseInt(10),req.body.phrase).write(`images/transform/${date}${req.file.filename}`)
+            fs.unlinkSync(`images/uploads/${req.file.filename}`)
+            sendImage(res, `${date}${req.file.filename}` )
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.sendStatus(400)
+        })
+    }else{
+        res.sendStatus(400)
+    }
 })
 
 app.listen(port, ()=>{
