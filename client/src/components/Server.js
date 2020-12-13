6c37abee1436c00f9eaf25c672767f9fcddc68b9
johan-martinez <johan.martinez01@uptc.edu.co>
2020-12-13 17:07:02 -0500

    Server.js client update

--- client/src/components/Server.js
+++ client/src/components/Server.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import Alert from './Alert';
+import Success from './Success';
 
 class Server extends Component{
 
     constructor(props){
         super(props)
+        this.state={
+            server:""
+        }
         this.handleInput=this.handleInput.bind(this)
         this.onSubmit=this.onSubmit.bind(this)
     }
@@ -19,18 +23,23 @@ class Server extends Component{
         if (e) {
             e.preventDefault()
         }
-        fetch(`${this.props.server}server`, {
-            method: 'post',
-            body: JSON.stringify({}),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => {
-            return response.json()
-        }).then((data) => {
-            this.successDiv.setAlert('El servidor se ha creado')
-        }).catch((e) => this.alertDiv.setAlert('Ha ocurrido un problema con el servidor'));
+        if(this.state.server!==""){
+            let {server} =this.state
+            fetch(`${this.props.server}server`, {
+                method: 'post',
+                body: JSON.stringify({server:server}),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                return response.json()
+            }).then((data) => {
+                this.successDiv.setAlert('El servidor se ha creado')
+            }).catch((e) => this.alertDiv.setAlert('Ha ocurrido un problema con el servidor'));
+        }else{
+            this.alertDiv.setAlert("Debe llenar el nombre de la instancia")
+        }
     }
 
     render(){
@@ -38,11 +47,17 @@ class Server extends Component{
         return(
             <div className='row justify-content-center h-100'>
                 <div className="col-sm-8 align-self-center text-left">
+                    <Alert ref={e=>{this.alertDiv=e}}/>
+                    <Success ref={e=>{this.successDiv=e}}/>
                     <div className="card-header text-center">
-                        <Alert ref={e=>{this.alertDiv=e}}/>
+
                         <h5 className="card-title">AGREGA UN SERVIDOR</h5>
                     </div>
                     <br/>
+                    <div className='form-group'>
+                        <label >Nombre:</label>
+                        <input onChange={this.handleInput} className='form-control' type="text" id="phrase" placeholder='Ingresa un nombre para la instancia' name="server"/>
+                    </div>
                     <br/>
                     <div className='card-footer text-center'>
                         <button onClick={this.onSubmit} className="btn btn-primary" >ENVIAR</button>
