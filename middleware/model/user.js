8f6ef987bf6457caccd1377b24b53d674fd0da9e
maicol <mmoarias4@gmail.com>
2021-02-16 12:28:55 -0500

    .

--- middleware/model/user.js
+++ middleware/model/user.js
@@ -24,26 +24,45 @@ var mailOptions = {
 // save email
 route.post('/', async (req, res) => {
     const { email } = req.body;
-    let user = {};
-    user.email = email;
-    let userModel = new User(user);
-    await userModel.save();
-    res.json(userModel);
+    if (! await exist(email)) {
+        let user = {};
+        user.email = email;
+        let userModel = new User(user);
+        await userModel.save();
+        res.json(userModel);
+    } else {
+        res.sendStatus(400);
+    }
 });
+
+route.get('/', async (req, res) =>{
+    var result = await getAllEmails();
+    res.json({emails: result});
+});
+
+async function exist(email) {
+    var result = await (await User.find({ email: email })).length;
+    return result > 0;
+}
+
 
 // get all emails
 async function serverFailed(server_failed) {
     console.log('SERVER FAILED. SENDING EMAILS... ')
-    let result = await User.find({});
-    if (result.length != 0) {
-        let emails = result.map(x => x.email);
+    let emails = await getAllEmails();
+    if (emails.length > 0) {
         mailOptions.to = emails.join(', ');
         mailOptions.text = `Hola! se le informa que el servidor con ip ${server_failed.ip} lanzado en el puerto ${server_failed.port} no responde.`;
-        await transporter.sendMail(mailOptions, (error, info) => { 
-            if (error) console.log('ERROR TO SEND EMAIL')
-            else console.log('SUCCESS! EMAIL SEND')
-         });
+        await transporter.sendMail(mailOptions, (error, info) => {
+            if (error) console.log('ERROR TO SEND EMAILS')
+            else console.log('SUCCESS! EMAILS SEND')
+        });
     }
+}
+
+async function getAllEmails() {
+    let result = await (await User.find({})).map(x => x.email);
+    return result;
 }
 
 module.exports = { route, serverFailed };
